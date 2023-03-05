3c839227c0651b35ec928d85d344001ced5b1b36
baejb <bjb6478@naver.com>
2023-03-06 02:09:02 +0900

    예산

---
+++ 예산.js
@@ -0,0 +1,16 @@
+function solution(d, budget) {
+    var answer =0;
+    let sum = 0;
+    d.sort(function(a, b) {
+    return a - b;
+    });
+    console.log(d);
+    for(let i = 0; i<d.length; i++){
+        sum+= d[i];
+        if(sum<=budget){
+            answer++;
+        }
+    }
+    return answer;
+}
+// solution([1,3,2,5,4,],9);
